86195b23d9459b60d0b01047f5470360636443f8
Junda Lou <loudakkk@hotmail.com>
2021-05-15 15:59:58 -0700

    add lc 344, 704

---
+++ 344_Reverse_String/two-pointer.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int l = 0, r = s.length - 1;
+        
+        while (l < r) {
+            char tmp = s[l];
+            s[l] = s[r];
+            s[r] = tmp;
+            
+            l++;
+            r--;
+        }
+    }
+}
